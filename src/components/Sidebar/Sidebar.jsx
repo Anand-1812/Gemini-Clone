e1b2e490ff36cd3fdeeb1af4edd12a3718f5a2e1
Anand <anand.work2005@gmail.com>
2025-08-10 23:20:54 +0530

    create the sidebar components

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,35 +1,42 @@
 import './Sidebar.css';
 import { assets } from '../../assets/assets'
+import { useState } from 'react';
 
 export const Sidebar = () => {
+
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+
   return (
     <div className="sidebar">
       <div className='top'>
-        <img className='menu' src={assets.menu_icon} ult="" />       
+        <img onClick={() => setToggleSidebar(prev => !prev)} className='menu' src={assets.menu_icon} ult="" />       
         <div className="new-chat">
-          <img src={assets.plus_icon} alt="" />
-          <p>New Chat</p>
+          <img src={assets.plus_icon}  alt="" />
+          { toggleSidebar ? <p>New Chat</p> : null }
         </div>
-        <div className="recent">
-          <p className='recent-title'>Recent</p>
-          <div className="recent-entry">
-            <img src={assets.message_icon} alt="" />
-            <p>What is react...</p>
+        { toggleSidebar ? 
+          <div className="recent">
+            <p className='recent-title'>Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>What is react...</p>
+            </div>
           </div>
-        </div>
+          : null
+        }
       </div>
       <div className='bottom'>
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="" />
-          <p>Help</p>
+          { toggleSidebar ? <p>Help</p> : null }
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="" />
-          <p>Activity</p>
+          { toggleSidebar ?  <p>Activity</p> : null } 
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="" />
-          <p>Settings</p>
+          { toggleSidebar ? <p>Settings</p> : null }
         </div>
       </div>
     </div>
